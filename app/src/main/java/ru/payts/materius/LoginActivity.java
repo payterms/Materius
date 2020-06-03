2bc9add3fe0d81f0e7ef444d4bef84c14d5b033f
Michael V. Gritsenko <payterms@mail.ru>
2020-06-03 18:56:02 +0300

    Login Activity added

---
+++ app/src/main/java/ru/payts/materius/LoginActivity.java
@@ -0,0 +1,61 @@
+package ru.payts.materius;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class LoginActivity extends AppCompatActivity {
+    Button loginButton;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        prepareFullScreenWindow();
+        setContentView(R.layout.activity_login);
+        InitViews();
+
+    }
+
+    private void InitViews() {
+        loginButton = findViewById(R.id.buttonLogin);
+        editText = findViewById(R.id.editTextName);
+        loginButton.setOnClickListener(view -> {
+            String nickname = editText.getText().toString();
+            if (nickname.isEmpty()){
+                Snackbar.make(view, "Field couldn't be empty!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+            else
+            {// Start Basic Activity
+                startDetailsActivity(nickname);
+            }
+
+        });
+    }
+
+    private void prepareFullScreenWindow() {
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ActionBar actionBar = getSupportActionBar ();
+        if (actionBar!=null){
+            actionBar.hide();
+        }
+    }
+
+    public void startDetailsActivity(String nickname) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("nickname", nickname);
+        startActivity(intent);
+    }
+
+}
